beb89131afb5a05bff0f6d8a7ee53fc31484c1a0
saadeddin <sonnysfo@yahoo.com>
2015-11-16 17:43:46 -0800

    pyc file

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='sybil',
+    name='sibyl',
     version='0.1.0',
     author='StartupML Nov Cohort 2015',
 
-    packages=['sybil'],
+
     license='LICENSE.txt',
     description='Categorical Attributes - Anomaly Detection.',
     long_description=open('README.txt').read(),
+    packages=['sibyl'],
 
 )
