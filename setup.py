d481ae2e065aa3dc3f382e52c2eb8ca1c3910ccb
saadeddin <sonnysfo@yahoo.com>
2015-11-16 18:07:59 -0800

    added package for pyc

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sibyl',
+    version='0.1.0',
+    author='StartupML Nov Cohort 2015',
+
+
+    license='LICENSE.txt',
+    description='Categorical Attributes - Anomaly Detection.',
+    long_description=open('README.txt').read(),
+    packages=['sibyl'],
+
+)
