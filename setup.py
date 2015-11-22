c9d251d6d2ba1e8b2dc83fc4315916e129da4b2a
saadeddin <sonnysfo@yahoo.com>
2015-11-21 18:08:14 -0800

    added more required packages to setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import find_packages
 
 setup(
     name='sibyl',
-    version='0.1.0',
+    version='0.1',
     author='StartupML',
     author_email='team@startup.ml',
     license='LICENSE.txt',
-    install_requires=['pandas'],
+    install_requires=['pandas','astroML','wget','astroML_addons'],
     description='Anomaly Detection in High Dimensional Heterogeneous Datasets',
     packages=find_packages())
