ca5bf69f4473ae0760a51d4cbdd75a99ed33eba4
Arshak Navruzyan <arshakn@gmail.com>
2015-11-21 19:02:15 -0800

    more printing of status in mushroom

--- examples/mushroom_ex.py
+++ examples/mushroom_ex.py
@@ -15,13 +15,16 @@ https://archive.ics.uci.edu/ml/datasets.html
 
 # Read the mushroom dataset from UC Irvine Dataset Repository
 if (not os.path.isfile("agaricus-lepiota.data")):
+    print "Downloading dataset ..."
     url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/mushroom/agaricus-lepiota.data'
     filename = wget.download(url)
 mushroom_data = pandas.read_csv("agaricus-lepiota.data",header = None)
 
 # Return anomaly score for every sample in the dataset, sample 50 times and include 100 instances in each sample
 mushroom_sibyl = Sibyl(mushroom_data)
+print "Training model"
 score_vec = mushroom_sibyl.score_dataset(50, 100)
+print "Model training completed!"
 
 # convert to numpy array to plot the histogram of the scores
 score_array = score_vec.values
@@ -37,7 +40,7 @@ anomaly_score_highest = score_vec.argmax()
 max_anomaly_score = mushroom_sibyl.score_instance(anomaly_score_highest, 50, 100)
 
 # To check the most important features and pair of features for that instance
-print(mushroom_sibyl.get_feature_importance(anomaly_score_highest))
+# print(mushroom_sibyl.get_feature_importance(anomaly_score_highest))
 # outputs: most important single feature, and most important feature-pairs, in terms of contribution to the total anomaly score for the instance with the index "anomaly_score_highest"
 '''
 {'single feature': ([22], 3.9745596868884547), 'pair features': ([(16, 22)], 3.9745596868884547)}
